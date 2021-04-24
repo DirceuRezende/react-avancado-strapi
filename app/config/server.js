86d1a3a7ed2f85bf81fd22aa91da978394be42ac
root <root@DESKTOP-7OA5N1N.localdomain>
2021-04-24 14:58:27 -0300

    Adicionado Strapi

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e066768118aad53df72d408db0fc090b'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '154a3937-ddc4-4a45-b5bc-4668e949c800'
+};
